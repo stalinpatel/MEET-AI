142725020b860e8502f35c822fd34ab7fe1ee17e
Stalin Patel <stalinpatel023@gmail.com>
2025-06-16 14:48:11 +0530

    04: authentication ui

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,8 @@
+import { SignInView } from "@/modules/auth/ui/views/sign-in-view"
+const Page = () => {
+  console.log("Sign in page server")
+
+  return (<SignInView/>)
+}
+
+export default Page
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,8 @@
+import { SignUpView } from "@/modules/auth/ui/views/sign-up-view"
+
+const Page = () => {
+  console.log("Sign up page server")
+  return (<SignUpView/>)
+}
+
+export default Page
