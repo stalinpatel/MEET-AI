27a827016cf4db093d144a8bde202f1055b9f58e
Stalin Patel <stalinpatel023@gmail.com>
2025-06-25 10:47:32 +0530

    08: trpc setup

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,35 +1,20 @@
 "use client";
-import { authClient } from "@/lib/auth-client";
-import { useRouter } from "next/navigation";
+
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
 
 export default function HomeView() {
-  const { data: session } = authClient.useSession();
-  const router = useRouter();
-  const { signOut } = authClient;
-
-  if (!session) {
-    return <p>Loading...</p>;
-  }
-
-  if (session) {
-    return (
-      <div className="flex flex-col p-4 gap-y-4">
-        <h1>Logged in as {session?.user?.name}</h1>
-        <button
-          className="p-2 bg-primary rounded-xl border-1  text-white"
-          onClick={() =>
-            signOut({
-              fetchOptions: {
-                onSuccess: () => {
-                  router.push("/sign-in"); // redirect to login page
-                },
-              },
-            })
-          }
-        >
-          Logout
-        </button>
-      </div>
-    );
-  }
+  const trpc = useTRPC();
+  const { data: helloData } = useQuery(
+    trpc.hello.queryOptions({ text: "Stalin" })
+  );
+  const { data: gdbyData } = useQuery(
+    trpc.goodbye.queryOptions({ text: "Milan" })
+  );
+  return (
+    <div className="flex flex-col p-4 gap-y-4">
+      <div>{helloData?.greeting}</div>
+      <div>{gdbyData?.farewell}</div>
+    </div>
+  );
 }
